8a264439c2f23ab980fe713f3e6c261ec7b6bdfa
Abha Jha <jhaabha08@gmail.com>
2020-07-13 19:06:07 +0530

    added individual projects

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import CompanyProjects from './components/CompanyProjects/companyProjects'
 import Navbar from './components/Navbar/navbar'
+import {Route,Switch,BrowserRouter} from 'react-router-dom'
+import ProjectDescription from './components/ProjectDescription/projectDescription'
+import CompanyProject from './components/CompanyProjects/companyProjects'
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      <Navbar/>
-      <CompanyProjects />
+        <Navbar/>
+        <Switch>
+          <Route exact path="/" component={CompanyProject}></Route>
+          <Route exact path="/project/:id" component={ProjectDescription}></Route>
+        </Switch>
     </div>
+    </BrowserRouter>
   );
 }
 
