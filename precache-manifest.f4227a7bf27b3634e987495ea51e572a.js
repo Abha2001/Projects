5b3e4650f7952bf1fff496a424fc821b93b186a3
Abha Jha <jhaabha08@gmail.com>
2020-07-13 19:24:38 +0530

    Updates

---
+++ precache-manifest.f4227a7bf27b3634e987495ea51e572a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f20939afb1f7b701fa86e06a2f90cd5",
+    "url": "/Projects/index.html"
+  },
+  {
+    "revision": "4236897d5aeee55366f2",
+    "url": "/Projects/static/css/main.7a56e332.chunk.css"
+  },
+  {
+    "revision": "4eb6fcdd92e0e13962da",
+    "url": "/Projects/static/js/2.bcf85881.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Projects/static/js/2.bcf85881.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4236897d5aeee55366f2",
+    "url": "/Projects/static/js/main.590c8d65.chunk.js"
+  },
+  {
+    "revision": "6881df174c550361b310",
+    "url": "/Projects/static/js/runtime-main.4582bed2.js"
+  }
+]);
